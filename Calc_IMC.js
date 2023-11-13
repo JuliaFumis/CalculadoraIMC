fa838b54a1e33bf5edfa292ec688d3c686f7a1b4
Julia-Fumis <jujufumis99@gmail.com>
2023-11-13 16:19:58 -0300

    Implementando classificador de IMC

--- Calc_IMC.js
+++ Calc_IMC.js
@@ -9,8 +9,19 @@ const calcIMC = () => {
     rl.question('Informe o peso (em kg): ', (peso) => {
         rl.question('Informe a altura (em metros): ', (altura) => {
             const imc = peso / (altura * altura);
-            console.log(`O IMC é: ${imc.toFixed(2)}`);
+            console.log(`O IMC é: ${imc.toFixed(1)}`);
             rl.close();
+            if (imc < 17) {
+                console.log("Muito abaixo do peso")
+            } else if (imc < 18.5) {
+                console.log("Abaixo do peso")
+            } else if (imc < 25) {
+                console.log("Peso normal")
+            } else if (imc < 30) {
+                console.log("Acima do peso")
+            } else {
+                console.log("Obesidade")
+            }
         });
     });
 }
